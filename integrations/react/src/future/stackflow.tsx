2e6aeca9ae29445d52154b7c0f8459385e6b7b37
Tony Won <tony@daangn.com>
2024-12-10 17:46:32 +0900

    feat(react): add `{ load: ... }` definition on react components map

--- integrations/react/src/future/stackflow.tsx
+++ integrations/react/src/future/stackflow.tsx
@@ -10,12 +10,13 @@ import {
   makeEvent,
 } from "@stackflow/core";
 import React, { useMemo } from "react";
+import type { ActivityComponentType } from "../__internal__/ActivityComponentType";
 import MainRenderer from "../__internal__/MainRenderer";
 import { makeActivityId } from "../__internal__/activity";
 import { CoreProvider } from "../__internal__/core";
 import { PluginsProvider } from "../__internal__/plugins";
 import { isBrowser, makeRef } from "../__internal__/utils";
-import type { ActivityComponentType, StackflowReactPlugin } from "../stable";
+import type { StackflowReactPlugin } from "../stable";
 import type { Actions } from "./Actions";
 import { ConfigProvider } from "./ConfigProvider";
 import type { StackComponentType } from "./StackComponentType";
@@ -31,7 +32,9 @@ export type StackflowPluginsEntry =
 export type StackflowInput<
   T extends ActivityDefinition<string>,
   R extends {
-    [activityName in T["name"]]: ActivityComponentType<any>;
+    [activityName in T["name"]]:
+      | ActivityComponentType<any>
+      | { load: () => Promise<{ default: ActivityComponentType<any> }> };
   },
 > = {
   config: Config<T>;
@@ -48,7 +51,9 @@ export type StackflowOutput = {
 export function stackflow<
   T extends ActivityDefinition<string>,
   R extends {
-    [activityName in T["name"]]: ActivityComponentType<any>;
+    [activityName in T["name"]]:
+      | ActivityComponentType<any>
+      | { load: () => Promise<{ default: ActivityComponentType<any> }> };
   },
 >(input: StackflowInput<T, R>): StackflowOutput {
   const plugins = [
@@ -59,7 +64,7 @@ export function stackflow<
     /**
      * `loaderPlugin()` must be placed after `historySyncPlugin()`
      */
-    loaderPlugin(input.config),
+    loaderPlugin(input),
   ];
 
   const enoughPastTime = () =>
